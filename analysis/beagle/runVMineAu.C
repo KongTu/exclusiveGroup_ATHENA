e185313ec225960a6afe26703cd07ef5de88457e
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-08-04 13:46:41 -0400

    initial commit

--- analysis/beagle/runVMineAu.C
+++ analysis/beagle/runVMineAu.C
@@ -73,7 +73,7 @@ void runVMineAu(const TString filename="eA_TEST", const int nEvents = 40000){
 	/* first  index VM species, rho=0, phi=1, jpsi=2*/
 	/* second index VM property, pt=0, eta=1, phi=2, theta=3, reserved=4*/
 	double bin_lower[]={0.,-8.,0.,0.,0.};
-	double bin_upper[]={5.0,8.,6.5,100.,10.};
+	double bin_upper[]={5.0,8.,6.5,100.,1.};
 	TH1D* h_VM[3][5];
 	for(int ivm=0;ivm<3;ivm++){
 		for(int ipro=0;ipro<5;ipro++){
@@ -118,8 +118,8 @@ void runVMineAu(const TString filename="eA_TEST", const int nEvents = 40000){
 
 		//event cuts
 		if( event_process != 91 && event_process != 93 ) continue;
-		if( trueQ2 < 1. || trueQ2 > 20. ) continue;
-		if( trueY > 0.95 || trueY < 0.01 ) continue;
+		// if( trueQ2 < 1. || trueQ2 > 20. ) continue;
+		// if( trueY > 0.95 || trueY < 0.01 ) continue;
 
 		//do analysis, or fill historgrams for event levels
 		h_trueT->Fill(-t_hat);
@@ -129,7 +129,7 @@ void runVMineAu(const TString filename="eA_TEST", const int nEvents = 40000){
 		//phi = 333, decay->kk
 		//jpsi = 443, nodecay
 		int pdglist[]={113,333,443};
-		int nobamlist[]={1,2,2};
+		int statuslist[]={1,2,2};
 		for(int j(0); j < nParticles; ++j ) {
 
 			const erhic::ParticleMC* particle = event->GetTrack(j);
@@ -151,12 +151,12 @@ void runVMineAu(const TString filename="eA_TEST", const int nEvents = 40000){
 			//do analysis track-by-track
 			for(int ivm=0;ivm<3;ivm++){
 				if(pdg!=pdglist[ivm]) continue;
-				if(status!=nobamlist[ivm])
+				if(status!=statuslist[ivm])
 				h_VM[ivm][0]->Fill(pt);
 				h_VM[ivm][1]->Fill(eta);
 				h_VM[ivm][2]->Fill(phi);
 				h_VM[ivm][3]->Fill(theta);
-				h_VM[ivm][4]->Fill(NoBAM);
+				h_VM[ivm][4]->Fill(-t_hat);
 			}
 
 		} // end of particle loop
