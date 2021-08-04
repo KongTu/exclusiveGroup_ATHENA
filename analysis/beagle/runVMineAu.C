400daa1e1683e681fc401e082777052df99cbd48
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-08-04 16:21:40 -0400

    initial commit

--- analysis/beagle/runVMineAu.C
+++ analysis/beagle/runVMineAu.C
@@ -74,7 +74,7 @@ void runVMineAu(const TString filename="eA_TEST", const int nEvents = 40000){
 	/* second  index VM species, rho=0, phi=1, jpsi=2*/
 	/* third   index VM property, pt=0, eta=1, phi=2, theta=3, reserved=4*/
 	double bin_lower[]={0.,-8.,0.,0.,0.};
-	double bin_upper[]={5.0,8.,6.5,100.,1.2};
+	double bin_upper[]={5.0,8.,6.5,4.,1.2};
 	TH1D* h_VM[2][3][5];
 	for(int ibreak=0;ibreak<2;ibreak++){
 		for(int ivm=0;ivm<3;ivm++){
@@ -163,7 +163,6 @@ void runVMineAu(const TString filename="eA_TEST", const int nEvents = 40000){
 			double rap = particle->GetRapidity();
 			double mass = particle->GetM();
 			double theta = particle->GetTheta(); 
-			// theta = theta*1000.0; //change to mrad;
 			double mom = particle->GetP();
 			int charge = particle->eA->charge;
 			int NoBAM = particle->eA->NoBam;
@@ -176,7 +175,6 @@ void runVMineAu(const TString filename="eA_TEST", const int nEvents = 40000){
 				h_VM[processindex][ivm][1]->Fill(eta);
 				h_VM[processindex][ivm][2]->Fill(phi);
 				h_VM[processindex][ivm][3]->Fill(theta);
-				cout << "theta~"<<theta<<endl;
 
 				//rho and phi daughters:
 				if(ivm<2){
@@ -193,12 +191,12 @@ void runVMineAu(const TString filename="eA_TEST", const int nEvents = 40000){
 					h_VM_daughter[processindex][ivm][0]->Fill(particle_daug1->GetPt());
 					h_VM_daughter[processindex][ivm][1]->Fill(particle_daug1->GetEta());
 					h_VM_daughter[processindex][ivm][2]->Fill(particle_daug1->GetPhi());
-					h_VM_daughter[processindex][ivm][3]->Fill(particle_daug1->GetTheta()*1000.);
+					h_VM_daughter[processindex][ivm][3]->Fill(particle_daug1->GetTheta());
 
 					h_VM_daughter[processindex][ivm][0]->Fill(particle_daug2->GetPt());
 					h_VM_daughter[processindex][ivm][1]->Fill(particle_daug2->GetEta());
 					h_VM_daughter[processindex][ivm][2]->Fill(particle_daug2->GetPhi());
-					h_VM_daughter[processindex][ivm][3]->Fill(particle_daug2->GetTheta()*1000.);
+					h_VM_daughter[processindex][ivm][3]->Fill(particle_daug2->GetTheta());
 
 				}
 			}
