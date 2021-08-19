22df367436d5ac371b8d580e4021957487bc201a
Kong Tu <kongkong@rice.edu>
2021-08-19 11:46:15 -0400

    add new stage 2 rootfiles

---
+++ analysis/beagle/runAllElect.sh
@@ -0,0 +1,3 @@
+root -l -q runVMineAu.C+\(\"/gpfs02/eic/DATA/BeAGLE/eAu/VM/18x110_Q2_1_20/EICTree/18x110_Q2_1_20_batch_3_20M/Output_input_temp_\*\"\,2e7\,0\,0\)
+
+root -l -q runVMineAu.C+\(\"/gpfs02/eic/DATA/BeAGLE/eAu/VM/18x110_Q2_1_20/EICTree/18x110_Q2_1_20_batch_3_20M/Output_input_temp_\*\"\,2e7\,0\,1\)
---
+++ analysis/beagle/runAllPhoto.sh
@@ -0,0 +1,3 @@
+root -l -q runVMineAu.C+\(\"/gpfs02/eic/DATA/BeAGLE/eAu/VM/18x110_Q2_0/EICTree/18x110_Q2_0_batch_1_80M/Output_input_temp_\*\"\,2e7\,1\,0\)
+
+root -l -q runVMineAu.C+\(\"/gpfs02/eic/DATA/BeAGLE/eAu/VM/18x110_Q2_0/EICTree/18x110_Q2_0_batch_1_80M/Output_input_temp_\*\"\,2e7\,1\,1\)
