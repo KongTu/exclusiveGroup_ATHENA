f0c57064a6fc3389f8b115192ed62c9ddca560f6
Kong Tu <kongkong@rice.edu>
2021-08-19 23:50:17 -0400

    finish all rootfiles

--- analysis/sartre/runAll.sh
+++ analysis/sartre/runAll.sh
@@ -2,7 +2,7 @@ root -l -q runSartreTree.C+\(0.2\,\"rho_photo\"\)
 root -l -q runSartreTree.C+\(0.2\,\"phi_photo\"\)
 root -l -q runSartreTree.C+\(0.2\,\"jpsi_photo\"\)
 
-root -l -q runSartreTree.C+\(0.2\,\"rho\"\)
-root -l -q runSartreTree.C+\(0.2\,\"phi\"\)
-root -l -q runSartreTree.C+\(0.2\,\"jpsi\"\)
+#root -l -q runSartreTree.C+\(0.2\,\"rho\"\)
+#root -l -q runSartreTree.C+\(0.2\,\"phi\"\)
+#root -l -q runSartreTree.C+\(0.2\,\"jpsi\"\)
 
