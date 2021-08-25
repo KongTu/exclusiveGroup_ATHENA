d651be135de3b9d4aedf456f5d32ec0f83e17964
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-08-25 13:49:40 -0400

    implement zhangbu's code in sartre

--- analysis/sartre/runSartreTree.C
+++ analysis/sartre/runSartreTree.C
@@ -54,9 +54,9 @@ ostream& operator<<(ostream& os, const TLorentzVector& v)
 //  Main function
 //===========================================================================
 
-void runSartreTree(double fractionOfEventsToRead = 1, TString vm_name="jpsi", bool PID_ = false, double lowPt_=0.1)
+void runSartreTree(double fractionOfEventsToRead = 1, TString vm_name="jpsi", bool PID_ = false, double setLowPt_=0.1)
 {
-    minPt_ = lowPt_;
+    minPt_ = setLowPt_;
     //
     //  Setup filenames
     //
