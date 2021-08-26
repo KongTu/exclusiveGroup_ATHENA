a2c6ceae35e41ce06d34037db621908b7879f26a
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-08-26 16:50:36 -0400

    start messing

--- analysis/EpIC/reader.cpp
+++ analysis/EpIC/reader.cpp
@@ -120,7 +120,7 @@ int main(int argc, char **argv) {
         pOut.Boost(-p_rf);
         pOut.Boost(p_rf_new);
    
-        TLorentzVector all = eIn+dIn-eOut-gammaOut-pOut-nIn_d;
+        TLorentzVector all = eIn+pIn+nIn_d-eOut-gammaOut-pOut-nIn_d;
         // PRINT4VECTOR(all,1);
         h_pz_diff->Fill(all.Pz());
         h_E_diff->Fill(all.E());
