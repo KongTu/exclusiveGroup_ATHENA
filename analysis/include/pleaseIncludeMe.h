2dc16709a292c5d7371aa072d2e16ac82937ab70
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-09-01 20:31:34 -0400

    adding smearing

--- analysis/include/pleaseIncludeMe.h
+++ analysis/include/pleaseIncludeMe.h
@@ -220,14 +220,20 @@ vector<TLorentzVector> letsMakeItReal(TLorentzVector e_beam, TLorentzVector e_sc
 		cout << "e pz " << e_scattered.Pz() << endl;
 		cout << "e E " << e_scattered.E() << endl;
 		TVector3 e_beam_boost = e_beam.BoostVector();
+
+		cout << "e beam lab px " << e_beam.Px() << endl;
+		cout << "e beam lab py " << e_beam.Py() << endl;
+		cout << "e beam lab pz " << e_beam.Pz() << endl;
+		cout << "e beam lab E " << e_beam.E() << endl;
+
 		double px = TMath::Sin(gRandom->Gaus(0.0,theta_resolution_e[0])*1E-3) * e_beam.Pz();
 		double py = TMath::Sin(gRandom->Gaus(0.0,theta_resolution_e[1])*1E-3) * e_beam.Pz();
 		
 		TLorentzVector e_beam_smear(px, py, e_beam.Pz(), sqrt(px*px+py*py+e_beam.Pz()*e_beam.Pz()+MASS_ELECTRON*MASS_ELECTRON));
-		cout << "e beam lab px " << e_beam_smear.Px() << endl;
-		cout << "e beam lab py " << e_beam_smear.Py() << endl;
-		cout << "e beam lab pz " << e_beam_smear.Pz() << endl;
-		cout << "e beam lab E " << e_beam_smear.E() << endl;
+		cout << "e beam lab smeared px " << e_beam_smear.Px() << endl;
+		cout << "e beam lab smeared py " << e_beam_smear.Py() << endl;
+		cout << "e beam lab smeared pz " << e_beam_smear.Pz() << endl;
+		cout << "e beam lab smeared E " << e_beam_smear.E() << endl;
 
 
 		TVector3 e_beam_reverse_boost = e_beam_smear.BoostVector();
