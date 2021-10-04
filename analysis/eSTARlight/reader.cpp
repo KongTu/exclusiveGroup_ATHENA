f0ce623e33297f624ca18a6974d203f7809e399f
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-10-04 14:08:30 -0400

    adding x-Q2 plots

--- analysis/eSTARlight/reader.cpp
+++ analysis/eSTARlight/reader.cpp
@@ -81,6 +81,7 @@ int main(int argc, char **argv) {
         if( fabs(phi.Rapidity()) > 4.0 ) continue;
 
         double Q2 = -gammaStar.Mag2();
+        cout << "Q2 " << Q2 << endl;
         double xbj = Q2 / (2*AIn.Dot(gammaStar));
         hQ2vsX->Fill(xbj,Q2);
 
