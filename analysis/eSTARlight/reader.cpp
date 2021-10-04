334a8a5b235f47b1d6e2aa1aef754c521431b6b9
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-10-04 13:30:11 -0400

    adding eSTARlight

--- analysis/eSTARlight/reader.cpp
+++ analysis/eSTARlight/reader.cpp
@@ -26,20 +26,8 @@ int main(int argc, char **argv) {
     }
 
     //histograms
-    std::vector<TH1D*> h_Q2(10, nullptr);
-    std::vector<TH1D*> h_t(10, nullptr);
-    std::vector<TH1D*> h_xB(10, nullptr);
-    std::vector<TH1D*> h_y(10, nullptr);
-    std::vector<TH1D*> h_phi(10, nullptr);
 
     TFile* output = new TFile("output.root","RECREATE");
-
-    h_Q2[0] = new TH1D("h_Q2_00", "", 90, 1., 10.);
-    h_t[0] = new TH1D("h_t_00", "", 50, 0.0, 1.0);
-    h_xB[0] = new TH1D("h_xB_00", "", 50, 0.0, 1.0);
-    h_y[0] = new TH1D("h_y_00", "", 50, 0.0, 1.0);
-    h_phi[0] = new TH1D("h_phi_00", "", 100, 0.0, 6.2831);
-
     //open file
     ReaderAscii inputFile(argv[1]);
     
@@ -57,30 +45,9 @@ int main(int argc, char **argv) {
         //if reading failed - exit loop
         if(inputFile.failed() ) break;
 
-
+        cout << "something " << iEvent << endl;
         //id
         iEvent++;
-    }
-    
-    //run info
-    std::shared_ptr<HepMC3::GenRunInfo> runInfo = inputFile.run_info();
-    
-    //run info
-    std::shared_ptr<HepMC3::Attribute> att_csValue = runInfo->attributes().find("integrated_cross_section_value")->second;
-    if(att_csValue){
-    	double csValue = std::stod(att_csValue->unparsed_string());
-    	std::cout << csValue << std::endl; 
-    }else{
-	std::cout << "HepMC3::Attribute pointer is NULL" << std::endl;
-    }
-    
-    //run info
-    std::shared_ptr<HepMC3::Attribute> att_eventsNumber = runInfo->attributes().find("generated_events_number")->second;
-    if(att_eventsNumber){
-        double eventsNumber = std::stod(att_eventsNumber->unparsed_string());
-        std::cout << eventsNumber << std::endl;
-    }else{
-	std::cout << "HepMC3::Attribute pointer is NULL" << std::endl;
     }
     
     //close file
